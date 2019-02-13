662771741c0114f8b9917dbdf96d01302b1c9188
Davidferpul <44580060+Davidferpul@users.noreply.github.com>
2019-02-13 20:54:18 +0100

    Update OrbitaKepler.m

--- Matlab/OrbitaKepler.m
+++ Matlab/OrbitaKepler.m
@@ -1,4 +1,4 @@
-%DefiniciÛn de constantes
+%Definici√≥n de constantes
 G=1.0;
 m1=1.0;
 m2=0.001;
@@ -10,15 +10,15 @@ t=linspace(0.0,100.0,N);
 y0=[1.0, 0.0, 0.0, 1.0];
 
 % Integracion con 'ode45' 
-orbit=@(t,U)[U(3);U(4);-mu*U(1)/sqrt(U(1)^2+U(2)^2);-mu*U(2)/sqrt(U(1)^2+U(2)^2)];
+orbit=@(t,U)[U(3);U(4);-mu*U(1)/(sqrt(U(1)^2+U(2)^2))^3;-mu*U(2)/(sqrt(U(1)^2+U(2)^2))^3];
 [t,U]=ode45(orbit,t,y0);
 
-r=U(:, 1:2);     % vector de posiciÛn                                                                      
+r=U(:, 1:2);     % vector de posici√≥n                                                                      
 v=U(:, 3:end);  % vector de velocidad   
 
-%gr·ficos
+%gr√°ficos
 figure
 plot(r(:,1),r(:,2))
 xlabel('x')
 ylabel('y')
-title('”rbita Kepleriana')
+title('√ìrbita Kepleriana')
